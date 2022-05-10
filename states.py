1f747ce1d14ce0017e27aaefba7c62f78f9c014f
matchanov07 <103366328+matchanov07@users.noreply.github.com>
2022-05-10 12:14:35 +0500

    Add files via upload

---
+++ states.py
@@ -0,0 +1,12 @@
+from aiogram.dispatcher.filters.state import StatesGroup, State
+
+
+class Ustoz(StatesGroup):
+    ism = State()
+    texno = State()
+    phone = State()
+    hudud = State()
+    Narxi = State()
+    ish   = State()
+    time  = State()
+    Maqsad= State()
